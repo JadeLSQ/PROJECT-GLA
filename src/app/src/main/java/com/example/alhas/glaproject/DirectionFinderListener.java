c8594f04ebe6f1271dba9b9884e072ba99ef0813
alhassana <alhassanabald.ab@gmail.com>
2017-05-10 23:44:42 +0200

    Delete DirectionFinderListener.java

--- src/app/src/main/java/com/example/alhas/glaproject/DirectionFinderListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.alhas.glaproject;
-
-import java.util.List;
-
-public interface DirectionFinderListener {
-    void onDirectionFinderStart();
-    void onDirectionFinderSuccess(List<Route> route);
-}
