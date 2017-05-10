388b008e497c0c1e2d5be9e767ed21e546a90a24
alhassana <alhassanabald.ab@gmail.com>
2017-05-11 00:02:10 +0200

    Update MainActivity.java

--- src/app/src/main/java/com/example/alhas/glaproject/MainActivity.java
+++ src/app/src/main/java/com/example/alhas/glaproject/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.alhas.glaproject;
 
 import android.app.Activity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.os.Message;
-        import android.view.View;
-        import android.widget.Button;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-        import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
 
 
 public class MainActivity extends Activity {
@@ -32,12 +38,32 @@ public class MainActivity extends Activity {
     CheckBox pref5;
     CheckBox pref6;
 
+    Button bot;
+    Button bot1;
+    int year_x, month_x, day_x;
+    static final int DIALOG_ID = 0;
+
+    int heur_x;
+    int minute_x;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latLongTV = (TextView) findViewById(R.id.latLongTV);
+    //    latLongTV = (TextView) findViewById(R.id.latLongTV);
+        final Calendar cal = Calendar.getInstance();
+        this.year_x = cal.get(Calendar.YEAR);
+        this.month_x = cal.get(Calendar.MONTH);
+        this.day_x = cal.get(Calendar.DAY_OF_MONTH);
+        AfficheDialogOnClick();
+
+        final Calendar cal2 = Calendar.getInstance();
+        this.heur_x = cal2.get(Calendar.HOUR);
+        this.minute_x = cal2.get(Calendar.MINUTE);
+
+        AfficheDialogOnClick1();
+
 
         add1 = (EditText) findViewById(R.id.adresse1);
         add2 = (EditText) findViewById(R.id.adresse2);
@@ -75,6 +101,98 @@ public class MainActivity extends Activity {
             }
         });
     }
+
+    public void AfficheDialogOnClick1(){
+
+
+        bot1 = (Button) findViewById(R.id.btn1);
+        bot1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                }
+        );
+    }
+
+    protected Dialog onCreateDialog1(int id) {
+
+        if (id == DIALOG_ID)
+
+            return new TimePickerDialog(this, Time, heur_x, minute_x, true);
+
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener Time = new TimePickerDialog.OnTimeSetListener() {
+
+
+        public void onTimeSet(TimePicker view, int heur, int minute) {
+
+            heur_x = heur;
+            minute_x = minute;
+
+            if (heur > Calendar.getInstance().get(Calendar.HOUR)) {
+                Toast.makeText(MainActivity.this, heur_x + ":" + minute_x, Toast.LENGTH_SHORT).show();
+            } else if (heur < Calendar.getInstance().get(Calendar.HOUR))
+            {
+                Toast.makeText(MainActivity.this, "Merci de Choisir une heure ulterieure s'il vous plait", Toast.LENGTH_LONG).show();
+            }
+            else if ( heur == Calendar.getInstance().get(Calendar.HOUR) && minute >= Calendar.getInstance().get(Calendar.MINUTE)){
+
+                Toast.makeText(MainActivity.this, heur_x + ":" + minute_x, Toast.LENGTH_LONG).show();
+            }
+
+            else if ( heur == Calendar.getInstance().get(Calendar.HOUR) && minute < Calendar.getInstance().get(Calendar.MINUTE)){
+
+                Toast.makeText(MainActivity.this, "Merci de Choisir une heure ulterieure s'il vous plait", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    private void AfficheDialogOnClick1() {
+    }
+
+    public void AfficheDialogOnClick() {
+
+        bot = (Button) findViewById(R.id.btn);
+        bot.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(DIALOG_ID);
+                    }
+                }
+        );
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        if (id == DIALOG_ID)
+            return new DatePickerDialog(this, Calendrier, year_x, month_x, day_x);
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener Calendrier = new DatePickerDialog.OnDateSetListener() {
+
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+            year_x = year;
+            month_x = month+1 ;
+            day_x = dayOfMonth;
+            if(year>=Calendar.getInstance().get(Calendar.YEAR) && month>=Calendar.getInstance().get(Calendar.MONTH) &&
+                    dayOfMonth>=Calendar.getInstance().get(Calendar.DAY_OF_MONTH) )
+                Toast.makeText(MainActivity.this, day_x+ "/" + month_x+"/" + year_x, Toast.LENGTH_LONG).show();
+            else {
+
+                Toast.makeText(MainActivity.this,"Merci de Choisir une date récente s'il vous plaît  ", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 
     private void sendRequest() {
         String adresse1 = add1.getText().toString();
